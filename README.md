e9c52c76268e34a69da9fb7549bb0a99b2069daf
simpart <taria.ki4m@gmail.com>
2021-09-29 06:14:48 +0000

    first

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # mofron-effect-width
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
 width effect for mofron
+
+
+# Install
+```
+npm install mofron mofron-effect-width
+```
+
+# Sample
+```html
+<setting>
+    <tag load="mofron-comp-frame">Frame</tag>
+    <tag load="mofron-effect-hrzpos">HrzPos</tag>
+    <tag load="mofron-effect-width">eWidth</tag>
+</setting>
+
+<Frame size=(0%,0.03rem) base-color=[100,100,255]>
+    <effect>
+        <HrzPos>center</HrzPos>
+        <eWidth eid=3 speed=500>0%,65%</eWidth>
+    </effect>
+</Frame>
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| | fromValue | size | start width |
+| | toValue | size | end width |
+
---
+++ index.js
@@ -0,0 +1,92 @@
+/**
+ * @file mofron-effect-width/index.js
+ * @brief width effect for mofron
+ * @license MIT
+ */
+module.exports = class extends mofron.class.Effect {
+    /**
+     * initialize effect
+     * 
+     * @param (mixed) 
+     *                key-value: effect config
+     * @short
+     * @type private
+     */
+    constructor (p1,p2) {
+        try {
+            super();
+            this.modname("Width");
+            this.shortForm("fromValue","toValue");
+            this.transition("width");
+            
+	    this.confmng().add("fromValue", { type: "size", init: "0rem" });
+	    this.confmng().add("toValue", { type: "size", init: "0rem" });
+            
+            this.beforeEvent(
+                (bf_eff) => {
+                    try {
+                        bf_eff.component().style({ 'width' : bf_eff.fromValue() });
+                    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }
+            );
+            
+            /* init config */
+	    if (0 < arguments.length) {
+                this.config(p1,p2);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * effect contents
+     * 
+     * @type private
+     */
+    contents (cmp) {
+        try {
+	    this.component().style({ 'width' : this.toValue() });
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    /**
+     * start width for animation
+     *
+     * @param (size) start width
+     * @return (size) start width
+     * @type parameter
+     */
+    fromValue (val) {
+        try {
+            return this.confmng("fromValue", val);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * end width for animation
+     *
+     * @param (size) end width
+     * @return (size) end width
+     * @type parameter
+     */
+    toValue (val) {
+        try {
+            return this.confmng("toValue", val);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+/* end of file */
